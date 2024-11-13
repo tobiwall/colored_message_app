d1b085936a871fd61cfc58ed50fb1ce12b55b5a0
tobiwall <t.w.95@web.de>
2024-11-13 16:32:21 +0100

    Save color number in a file

--- crystal-colors-main/src/main.rs
+++ crystal-colors-main/src/main.rs
@@ -23,16 +23,6 @@ mod msac;
 type LastMessages = Arc<HashMap<String, String>>;
 type Channels = Arc<HashMap<String, msac::Channel>>;
 
-// #[get("/<path..>")]
-// pub async fn serve(path: PathBuf) -> Option<NamedFile> {
-//     let mut path = Path::new(relative!("assets")).join(path);
-//     if path.is_dir() {
-//         path.push("index.html");
-//     }
-
-//     NamedFile::open(path).await.ok()
-// }
-
 #[get("/main.js")]
 pub async fn serve() -> Option<NamedFile> {
     NamedFile::open("assets/main.js").await.ok()
@@ -92,7 +82,6 @@ async fn echo_socket<'r>(
             }
 
             {
-                // let mut last_messages = last_messages.write().await;
                 let last_message = last_messages
                     .entry(room.clone())
                     .or_insert_with(|| "180".to_string());
@@ -123,7 +112,20 @@ async fn echo_socket<'r>(
                                     messages.push(new_message.clone());
                                     save_message_to_file(&messages).unwrap();
                                 },
-                                Err(_e) => continue,
+                                Err(_) => {
+                                    match serde_json::from_str::<i32>(&message) {
+                                        Ok(number) => {
+                                            // Handle the number (e.g., store it or use it in some way)
+                                            println!("Received a number: {}", number);
+                                            save_color(number.to_string()).unwrap();
+                                        },
+                                        // If it's neither a `SingleMessage` nor an integer, log the error
+                                        Err(e) => {
+                                            println!("Failed to parse message: {}", e);
+                                            continue;
+                                        }
+                                    }
+                                }
                                 };
                             }
                         } else {
@@ -183,6 +185,22 @@ fn save_message_to_file(messages: &Vec<SingleMessage>) -> Result<(), Error> {
     Ok(())
 }
 
+fn save_color(color: String) -> Result<(), Error> {
+    let file = match File::create("color.json") {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Faild to create a messages.json");
+            return Err(e);
+        }
+    };
+
+    if let Err(e) = serde_json::to_writer(file, &color) {
+        return Err(e.into());
+    }
+
+    Ok(())
+}
+
 fn read_messages_from_file() -> Result<Vec<SingleMessage>, io::Error> {
     let mut file = File::open("messages.json")?;
     let mut content = String::new();
@@ -192,6 +210,15 @@ fn read_messages_from_file() -> Result<Vec<SingleMessage>, io::Error> {
     Ok(messages)
 }
 
+// fn read_color_from_file() -> Result<Vec<SingleMessage>, io::Error> {
+//     let mut file = File::open("color.json")?;
+//     let mut content = String::new();
+//     file.read_to_string(&mut content)?;
+//     let messages = serde_json::from_str(&content)?;
+//     println!("This is the content{content}");
+//     Ok(messages)
+// }
+
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
     let all_messages = Arc::new(Mutex::new(read_messages_from_file().unwrap()));
@@ -200,11 +227,9 @@ async fn rocket() -> shuttle_rocket::ShuttleRocket {
         .mount("/", rocket::routes![serve, index, echo_socket]);
 
     // manage a mpmc channel for strings
-    // let channel = msac::Channel::new();
     let channels: Channels = Arc::new(HashMap::new());
     let rocket = rocket.manage(channels);
     // manage a sting for the last sent message
-    // let last_message = Arc::new(RwLock::new(String::new()));
     let last_messages: LastMessages = Arc::new(HashMap::new());
     let rocket = rocket.manage(last_messages);
 
